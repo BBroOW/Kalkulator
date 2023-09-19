2261ceda3afa9934c318c4067c5d2a3898870360
BBroOW <brage_hagelund@outlook.com>
2023-09-19 11:59:32 +0200

    Yippie, den fungerer

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-const display = getElementById("result")
+let nummer = ""
 
 
-document.getElementById("knapp1").addEventListener("click", function(e){
-    
-})
+function knapp(num){
+    document.getElementById("textInput").value += num
+    nummer += num
+}
+
+function symbol(symbol){
+    nummer += symbol
+    document.getElementById("textInput").value = ""
+    document.getElementById("textInput").placeholder = symbol
+}
+
+function results(){
+    document.getElementById("textInput").value = eval(nummer)
+    document.getElementById("textInput").placeholder = "0"
+}
+
+
+
+function reset(){
+    document.getElementById("textInput").value = ""
+    nummer = ""
+}
+
+
+
