2e7c92a303206aadebd005c5c0235b5605837925
BBroOW <brage_hagelund@outlook.com>
2024-02-19 08:57:01 +0100

    Refactor viewLog, knapp, symbol, results, and reset functions

--- index.js
+++ index.js
@@ -1,53 +1,44 @@
-let nummer = ""
-let log = []
+let nummer = "";
+let log = [];
 
-function viewLog(){
+function viewLog() {
+  document.getElementById("liste").innerHTML = "";
+  log.map((logi) => {
+    const el = document.createElement("li");
+    el.style.textAlign = "center";
+    el.innerText = logi.nummer + "=" + logi.result;
 
-    document.getElementById("liste").innerHTML = ""
-        log.map((
-            logi
-        ) =>{
-            const el = document.createElement("li")
-            el.style.textAlign = "center"
-            el.innerText = logi.nummer + "=" + logi.result
-
-            document.getElementById("liste").appendChild(el)
-        })
-
-}
-    
-
-function knapp(num){
-    document.getElementById("textInput").value += num
-    nummer += num
+    document.getElementById("liste").appendChild(el);
+  });
 }
 
-function symbol(symbol){
-    nummer += symbol
-    document.getElementById("textInput").value = ""
-    document.getElementById("textInput").placeholder = symbol
+function knapp(num) {
+  document.getElementById("textInput").value += num;
+  nummer += num;
 }
 
-function results(){
-    document.getElementById("textInput").value = eval(nummer)
-    document.getElementById("textInput").placeholder = "0"
-    log.push(
-        {
-            result:eval(nummer), tid:new Date(), nummer
-        }
-    )
-    viewLog()
-    console.log(log)
+function symbol(symbol) {
+  nummer += symbol;
+  document.getElementById("textInput").value = "";
+  document.getElementById("textInput").placeholder = symbol;
 }
 
-
-
-function reset(){
-    document.getElementById("textInput").value = ""
-    document.getElementById("textInput").placeholder = "0"
-    nummer = ""
+function results() {
+  document.getElementById("textInput").value = eval(nummer);
+  document.getElementById("textInput").placeholder = "0";
+  log.push({
+    result: eval(nummer),
+    tid: new Date(),
+    nummer,
+  });
+  viewLog();
+  console.log(log);
 }
 
+function reset() {
+  document.getElementById("textInput").value = "";
+  document.getElementById("textInput").placeholder = "0";
+  nummer = "";
+}
 
-
-console.log(log)
+console.log(log);
